d48723809b1ecf8939ad8682ca0ec10612db4e1b
kunalkarpe <kunalkarpe@fyntune.com>
2023-12-07 17:11:52 +0530

    completed custom filtering

--- src/pages/Mockuser/MockUser.jsx
+++ src/pages/Mockuser/MockUser.jsx
@@ -3,6 +3,8 @@ import mdata from "@src/MocakData/MOCK_DATA";
 import { FaRegArrowAltCircleUp } from "react-icons/fa";
 import { FaArrowCircleDown } from "react-icons/fa";
 import { useState } from "react";
+import { BsThreeDots } from "react-icons/bs";
+import { FaFilter } from "react-icons/fa";
 import {
   flexRender,
   useReactTable,
@@ -15,18 +17,19 @@ const MockUser = () => {
   const data = useMemo(() => mdata, []);
   const [sorting, setSorting] = useState([]);
   const [filtering, setFiltering] = useState("");
-
+  const [fetchedData, setFetchedData] = useState();
+  const [toogle, setToogle] = useState(false);
   const columns = [
     {
       header: "ID",
       accessorKey: "id",
     },
     {
-      header: "First name",
+      header: "First Name",
       accessorKey: "first_name",
     },
     {
-      header: "Last name",
+      header: "Last Name",
       accessorKey: "last_name",
     },
     {
@@ -36,6 +39,24 @@ const MockUser = () => {
     {
       header: "Gender",
       accessorKey: "gender",
+    },
+    {
+      header: "Button",
+      accessorKey: "button",
+      cell: ({ row }) => {
+        return (
+          <>
+            <button
+              onClick={() => {
+                setFetchedData(JSON.stringify(row.original.first_name)),
+                  alert(`Hello from ${row.original.first_name} `);
+              }}
+            >
+              <BsThreeDots />
+            </button>
+          </>
+        );
+      },
     },
   ];
 
@@ -53,7 +74,15 @@ const MockUser = () => {
     onSortingChange: setSorting,
     onGlobalFilteringChange: setFiltering,
   });
-  //   console.log(column);
+  // console.log(fetchedData);
+
+  const handleToogle = () => {
+    if (toogle == false) {
+      setToogle(true);
+    } else {
+      setToogle(false);
+    }
+  };
 
   return (
     <>
@@ -66,16 +95,24 @@ const MockUser = () => {
             className="border border-slate-300 rounded-lg shadow-md mb-2 p-1 outline-none"
             placeholder="Search here...."
           />
+          <button
+            className={`border border-black ms-12 rounded-lg p-1 ${
+              toogle ? "bg-lime-300" : ""
+            }`}
+            onClick={handleToogle}
+          >
+            <FaFilter />
+          </button>
+
           <table>
             <thead className="border border-black rounded-2xl">
               {table.getHeaderGroups().map((headerGroup) => (
                 <tr
                   key={headerGroup.id}
-                  className="border border-black rounded-2xl"
+                  className="border border-slate-400 bg-slate-200 rounded-2xl py-4"
                 >
                   {headerGroup.headers.map((header) => (
                     <th
-                      className="b d"
                       key={header.id}
                       onClick={header.column.getToggleSortingHandler()}
                     >
@@ -147,6 +184,31 @@ const MockUser = () => {
             </button>
           </div>
         </div>
+        {toogle ? (
+          <div className="flex flex-col relative z-20 border border-black w-32 bg-white ms-12 h-fit py-2 shadow rounded">
+            <div className="px-1 border-b border-black items-center justify-center">
+              <label>Toggle </label>
+            </div>
+            {table.getAllLeafColumns().map((column) => {
+              return (
+                <div key={column.id} className="px-1">
+                  <label>
+                    <input
+                      {...{
+                        type: "checkbox",
+                        checked: column.getIsVisible(),
+                        onChange: column.getToggleVisibilityHandler(),
+                      }}
+                    />{" "}
+                    {column.id}
+                  </label>
+                </div>
+              );
+            })}
+          </div>
+        ) : (
+          ""
+        )}
       </div>
     </>
   );
